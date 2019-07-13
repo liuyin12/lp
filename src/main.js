0e5641763b581393f992d2754821cdd2ce1b6c99
ly <1990707901@qq.com>
2019-07-13 16:53:58 +0800

    login页面完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 导入UI包和样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 导入router
+// 在使用vue-cli的时候 @符号在路径中作为前缀使用,代表src目录
+import router from '@/router'
+// 导入axios
+// import axios from 'aixos'
+// Vue.prototype.$http = axios
+import axios from 'axios'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
+// 注册到vue实例
+Vue.use(ElementUI)
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
